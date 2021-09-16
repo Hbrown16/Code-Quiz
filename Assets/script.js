61cc5c8ebfbaac57751acb21b8eb7aa8e572f4cb
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 18:15:19 -0400

    query question

--- Assets/script.js
+++ Assets/script.js
@@ -2,3 +2,4 @@ var timerEL = document.getElementById("timer");
 var startButton = document.getElementById("start");
 var scoreBoard = document.getElementsByClassName('scores');
 var scoreText = document.getElementById('score-count');
+var question = document.querySelector('#question');
