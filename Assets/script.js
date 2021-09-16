f2fdac0968c36de256307ae85ddc4f59e8f4f726
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 18:16:17 -0400

    answers

--- Assets/script.js
+++ Assets/script.js
@@ -3,3 +3,4 @@ var startButton = document.getElementById("start");
 var scoreBoard = document.getElementsByClassName('scores');
 var scoreText = document.getElementById('score-count');
 var question = document.querySelector('#question');
+var answers = Array.from(document.querySelectorAll('.answer-text'));
