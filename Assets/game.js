cbf82e6e8f18e4108bd53a89c24468b773b732b3
Hbrown16 <hbrown1094@gmail.com>
2021-09-16 20:02:59 -0400

    var

--- Assets/game.js
+++ Assets/game.js
@@ -1,2 +1,6 @@
 var platerName = document.querySelector('#player')
 var saveScoreBtn = document.querySelector('#saveScoreBtn')
+var finalScore = document.querySelector('#finalScore')
+var mostRecentScore = localStorage.getItem('mostRecentScore')
+var playAgain = document.getElementsByClassName('btn')
+
