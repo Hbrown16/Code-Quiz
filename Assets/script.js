10f019474ba3e0a4864d97476ef9eff840589f74
Hbrown16 <hbrown1094@gmail.com>
2021-09-19 14:10:56 -0400

    debug

--- Assets/script.js
+++ Assets/script.js
@@ -2,8 +2,8 @@ const { start } = require("@popperjs/core");
 
 var timerEL = document.getElementById("timer");
 var startButton = document.getElementById("start");
-var scoreBoard = document.getElementsByClassName('scores');
-var scoreText = document.getElementById('score-count');
+var scoreBoard = document.getElementsByClassName('scores')
+var scoreText = document.getElementById('score-count')
 var question = document.querySelector('#question');
 var answers = Array.from(document.querySelectorAll('.answer-text'));
 var currentQuestion = {};
